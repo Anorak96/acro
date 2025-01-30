22aa77406a3230f1005a4d521a568cd7262d9158
David Duyile <femiduyile@gmail.com>
2025-01-30 14:48:12 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -15,11 +15,12 @@ from django.contrib.auth import get_user_model
 import os
 
 User = get_user_model()
-email = os.getenv("DJANGO_SUPERUSER_EMAIL", "admin@mail.com")
+email = os.getenv("DJANGO_SUPERUSER_EMAIL", "admin@gmail.com")
 password = os.getenv("DJANGO_SUPERUSER_PASSWORD", "bavtwany")
+username = os.getenv("DJANGO_SUPERUSER_USERNAME", "acro")
 
 if not User.objects.filter(email=email).exists():
-    User.objects.create_superuser(email=email, password=password)
+    User.objects.create_superuser(email=email, password=password, username=username)
     print("Superuser created")
 else:
     print("Superuser already exists")
