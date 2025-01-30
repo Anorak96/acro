902dce55d435099979e4b8fb97cbfaca9713890d
David Duyile <femiduyile@gmail.com>
2025-01-30 14:38:58 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,6 +5,6 @@ set -o errexit   #  exit on error
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
-python manage.py makemigrations user
+python manage.py makemigrations users
 python manage.py makemigrations music
 python manage.py migrate
