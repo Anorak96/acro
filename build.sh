98b2d3ede60d66ea0ce287781d112dc0158a3adb
David Duyile <femiduyile@gmail.com>
2025-01-31 10:14:45 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,8 +5,7 @@ set -o errexit   #  exit on error
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
-python manage.py makemigrations users
-python manage.py makemigrations music
+python manage.py makemigrations
 python manage.py migrate
 
 # Create superuser
