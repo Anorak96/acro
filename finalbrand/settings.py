b83e9f82923adcd9795485540829af94ab356cf1
David Duyile <femiduyile@gmail.com>
2025-01-31 09:55:11 +0100

    Update settings.py

--- finalbrand/settings.py
+++ finalbrand/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # STORAGES = copy.deepcopy(global_settings.STORAGES)
 STORAGES = {
     "default": {
